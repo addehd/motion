f93f76e2fb2cf4e5f7bb906ebd628a2b2b65c48d
addehd <hej@adolfo.se>
2020-05-12 14:36:34 +0200

    small semantic fix

--- simulate.js
+++ simulate.js
@@ -1,12 +1,12 @@
 const motion = require('./motion.js')
-fs = require('fs')
+const fs = require('fs')
 const fileWithCommands = process.argv.pop()
 
 fs.readFile(fileWithCommands, 'utf8', function (err,data) {
   if (err) {
     return console.log(err) }
 
-  var commands = data
+  let commands = data
   let results = motion.simulate(commands)
   console.log(results)
 })
